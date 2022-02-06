9065e753ddd675622a828c8c1b65759c47a51522
krko <kristof.kox@gmail.com>
2022-02-06 12:03:32 +0100

    Implement currency

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,24 +1,32 @@
 package guru.springframework;
 
-import java.util.Objects;
-
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public abstract Money times(int multiplier);
 
-    public static Money dollar(int ammount) {
-        return new Dollar(ammount);
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
     }
 
-    public static Money franc(int ammount) {
-        return new Franc(ammount);
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
     }
 
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount && this.getClass().equals(o.getClass());
+    }
+
+    protected String currency() {
+        return currency;
     }
 
 //    @Override
